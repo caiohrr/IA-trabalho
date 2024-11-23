e4d69b628959beda8d788be71d74ca8649bedc79
CAIO HENRIQUE RAMOS RUFINO <chrr22@inf.ufpr.br>
2024-11-23 12:44:18 -0300

    Adicionando função para verificar todas as possibilidades

--- src/main.c
+++ src/main.c
@@ -2,14 +2,40 @@
 #include <stdlib.h>
 #include "utils.h"
 
+int iteration = 1;
+
+void generatePossibilities(int **board, int rows, int columns, int cell, int state) {
+
+        if (cell >= rows * columns) {
+                //printBoard(board, rows, columns);
+                return;
+        }
+        
+        int r, c;
+        iteration++;
+
+        r = cell / rows;
+        c = cell % rows;
+        //printf("%d %d\n", r, c);
+        board[r][c] = state;
+
+        generatePossibilities(board, rows, columns, cell + 1, 0);
+        generatePossibilities(board, rows, columns, cell + 1, 1);
+}
+
 int main() {
 
         int rows, columns;
         scanf("%d %d", &rows, &columns);
 
         int ** board = allocBoard(rows, columns);
+
         readBoard(board, rows, columns);
-        printBoard(board, rows, columns);
+        //printBoard(board, rows, columns);
+
+        generatePossibilities(board, rows, columns, 0, 0);
+
+        printf("%d\n", iteration);
 
         return 0;
 }
