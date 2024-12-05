5dbc1c19f750782c535d01195127753961c7b257
Caio H. R. R. <caiohrr@gmail.com>
2024-12-05 16:02:13 -0300

    Cláusula stagnation adicionada

--- src/main.cpp
+++ src/main.cpp
@@ -69,7 +69,7 @@ expr setDisjunction(context &z3_context, vector<short> &variables, bool negate) 
 
         // Itera sobre o conjunto de variáveis
         for (const auto &var : variables) {
-                // Create uma variável boolean com o nome "x_{var}"
+                // Gera uma variável boolean com o nome "x_{var}"
                 expr curr_var = z3_context.bool_const(("x_" + to_string(var)).c_str());
 
                 if (negate) {
@@ -137,14 +137,46 @@ void overcrowding(solver &z3_solver, context &z3_context, short x, set<short> &n
 
         vector<vector<short>> subsets = generateSubsets(neighbors, 4);
         for (auto &subset : subsets) {
-                expr disjunction = setDisjunction(z3_context, subset, true);
+                expr neg_disjunction = setDisjunction(z3_context, subset, true);
                 // Junta por conjunção cada disjunção
-                conjunction = conjunction && disjunction;
+                conjunction = conjunction && neg_disjunction;
         }
         z3_solver.add(conjunction);
         cout << "Overcrowding clause for " << "x_" << x << ":" << conjunction << "\n";
 }
 
+void stagnation(solver &z3_solver, context &z3_context, short x, set<short> &neighbors) {
+
+        expr conjunction = z3_context.bool_val(true);
+
+        vector<vector<short>> subsets = generateSubsets(neighbors, 2);
+        for (auto &subset : subsets) {
+                vector<short> diff_set = {};
+                set_difference(neighbors.begin(), neighbors.end(),
+                               subset.begin(), subset.end(),
+                               inserter(diff_set, diff_set.begin()));
+
+                //cout << "set diff:\n";
+                //for (auto const &elem : diff_set)
+                //        cout << elem << " ";
+                //cout << "\n";
+
+                // Negação da variável atual
+                expr curr_var = !z3_context.bool_const(("x_" + to_string(x)).c_str());
+
+                // Disjunção da negação do subconjunto de tamanho 2 atual
+                expr neg_disjunction = setDisjunction(z3_context, subset, true);
+
+                // Disjunção da negação da diferença de todos os vizinhos e o subconjunto
+                // de tamanho 2 atual
+                expr diff_disjunction = setDisjunction(z3_context, diff_set, false);
+
+                // Junta por conjunção cada disjunção
+                conjunction = conjunction && (curr_var || neg_disjunction || diff_disjunction);
+        }
+        z3_solver.add(conjunction);
+        cout << "Stagnation clause for " << "x_" << x << ":" << conjunction << "\n";
+}
 
 int main() {
 
@@ -177,7 +209,10 @@ int main() {
 
                         // Se a célula está viva
                         if (board[i][j] == 1) {
+                                cout << "life & preservation\n";
+                        } else {
                                 loneliness(z3_solver, z3_context, x, x_neighbors);
+                                stagnation(z3_solver, z3_context, x, x_neighbors);
                                 overcrowding(z3_solver, z3_context, x, x_neighbors);
                         }
                 }
